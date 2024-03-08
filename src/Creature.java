80531cddf31c7e6adcee7a8557e787926798dd64
Egor Girko <e.girko@ya.ru>
2024-03-08 10:43:24 +0000

    0

--- src/Creature.java
+++ src/Creature.java
@@ -13,15 +13,25 @@ class Creature {
         this.hp = hp;
     }
 
-    void attack(Creature enemy) { // метод атаки. врагом будет считаться любой недруг, если это герой, значит, его враг это гоблин или скелет, если это скелет или гоблин, то их враг это герой
-        while (true) {
-            if (enemy.hp > 0) {
-                if (agility < (Math.random() * 10)) {
-                    enemy.hp -= power;
-                    System.out.println(enemy.name + " получил ранение на " + power);
-                } else System.out.println(name + " промахнулся.");
 
+    void fight(Creature enemy) { // метод атаки. врагом будет считаться любой недруг, если это герой, значит, его враг это гоблин или скелет, если это скелет или гоблин, то их враг это герой
+        while (enemy.hp > 0) { // veroyatnost uvorota protivnika ishodya iz ego lovkosti
+            int hitPoint = (int) (this.power + Math.random() * 10) / 2;
+            if ((Math.random() * 10) > 8){
+                hitPoint *= 2;
+                System.out.println(enemy.name + " poluchaet kriticheskoe ranenie na " + hitPoint);
             }
+            if (hitPoint < 3) {
+                hitPoint += 6;
+            }
+            attack(enemy, hitPoint);
         }
+
+    }
+
+
+    void attack(Creature enemy, int hitPoint) {
+        enemy.hp -= hitPoint;
+        System.out.println(enemy.name + " получил ранение на " + hitPoint);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
-import java.util.Scanner;
+
 
 class Main {
 
     public static void main(String[] args) {
-        Creature hero = new Player("Сэр Лансеврот", 8, 7, 30, 45, 0);
-        Creature goblin = new Goblin("Гоблин", 5, 4, 10);
 
-        hero.attack(goblin);
-        goblin.attack(hero);
+
+
+        Creature hero = new Player("Сэр Лансеврот", 8, 7, 30, 45, 0);
+        Creature goblin = new Goblin("Гоблин", 5, 4, 100);
+        Creature skeleton = new Skeleton("Skeleton", 2, 3, 90);
+
+
+        while (true) {
+            hero.fight(goblin);
+
+        }
     }
+
+
+
+
 }
